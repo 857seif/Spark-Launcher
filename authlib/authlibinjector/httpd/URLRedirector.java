ab7274de629650a70b432ebe7d806f34c55801b8
857seif <saiftarek20101@gmail.com>
2026-08-25 22:03:55 +0300

    Delete authlib directory

--- authlib/authlibinjector/httpd/URLRedirector.java
+++
@@ -1,9 +0,0 @@
-
-package moe.yushi.authlibinjector.httpd;
-
-import java.util.Optional;
-
-public interface URLRedirector {
-    public Optional<String> redirect(String var1, String var2);
-}
-
--- authlib/authlibinjector/internal/org/json/simple/JSONAware.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package moe.yushi.authlibinjector.internal.org.json.simple;
-
-public interface JSONAware {
-    public String toJSONString();
-}
-
